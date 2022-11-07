b45016628946b073e4c5f58d63f5b9226e0baa4b
Breorkrat <70968315+Breorkrat@users.noreply.github.com>
2022-11-07 17:56:55 -0300

    Upload

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function sleep(ms) {  return new Promise(resolve => setTimeout(resolve, ms)) }
 		var sans = new Audio('sans.mp3')
 		
         //var bt = window.document.getElementById('bt')
-        var cx = window.document.getElementById('cx')
-        var dv = window.document.getElementById('dv')
+        var cx = document.getElementById('cx')
+        var dv = document.getElementById('dv')
+        var box = document.getElementById('hook')
+        var webhook = "https://ptb.discord.com/api/webhooks/802012648223932416/0EzcblvVPBdoxIgQ6lxaK08U1vIp17nnp75XeHuwhzpTssH4_rzh5UDgx5UiCR982G46"
+ 
         let txt;
  
         cx.addEventListener('keypress',  async (verif) => {
@@ -13,6 +16,12 @@ function sleep(ms) {  return new Promise(resolve => setTimeout(resolve, ms)) }
                 dv.innerHTML = ""
                 txt = cx.value.split("")
                 sans.play();
+
+
+                if(box.checked == true){
+                    enviarmsg(cx.value)
+                }
+
                 for(let i = 0; i < txt.length; i++){
                     dv.innerHTML += `${txt[i]}`
                         if(txt[i] !== " ") {
@@ -26,3 +35,18 @@ function sleep(ms) {  return new Promise(resolve => setTimeout(resolve, ms)) }
             }
 
         })
+
+        function enviarmsg() {
+            if (cx.value.length <= 2000) {
+                var msg = {
+                    "content": cx.value
+                }
+                fetch(webhook + "?wait=true",
+                {
+                    "method": "POST",
+                    "headers": { "content-type": "application/json" },
+                    "body": JSON.stringify(msg)
+                })
+                .then(a => a.json()).then(console.log)
+            }
+        }
